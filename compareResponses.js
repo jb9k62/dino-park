4286b875f9ee4e843a43a4acf8e06cdd63aa8a0b
jb9k62 <jb9k62@protonmail.com>
2023-02-27 22:41:01 +0100

    add script to check if API responses differ

---
+++ compareResponses.js
@@ -0,0 +1,32 @@
+let callCount = 0;
+const stripTime = (arr) =>
+  arr.map((a) => {
+    delete a.time;
+    return a;
+  });
+
+const call = () =>
+  fetch(
+    "https://5rz6r1it73.execute-api.eu-west-2.amazonaws.com/nudls/feed"
+  ).then((r) => r.json());
+
+call().then((r1) => {
+    callCount++;
+
+  const map = {
+    [callCount]: JSON.stringify(stripTime(r1)),
+  };
+
+  setInterval(
+    () =>
+      call().then((r) => {
+        callCount++;
+        map[callCount] = JSON.stringify(stripTime(r));
+
+        console.log(
+          `Current same as previous? ${map[callCount] === map[callCount - 1]}`
+        );
+      }),
+    200
+  );
+});
